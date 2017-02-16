896926ccad4f9d847c1d11c05140653668c28689
zhuyiqi_026@163.com <31415926zyq>
2017-02-17 00:03:40 +0800

    commit

--- todoApp/src/components/addTodo.js
+++ todoApp/src/components/addTodo.js
@@ -1,31 +1,53 @@
 /**
  * Created by SZL4ZSY on 2/16/2017.
  */
-import React, { Component, PropTypes } from 'react'
-
-export default class AddTodo extends Component {
-    render() {
+// import React, { Component, PropTypes } from 'react'
+//
+// export default class AddTodo extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <input type='text' ref='input' />
+//                 <button onClick={(e) => this.handleClick(e)}>
+//                     Add
+//                 </button>
+//             </div>
+//         )
+//     }
+//
+//     /**
+//      * 将需要添加的内容传给onAddClick然后清空value
+//      */
+//     handleClick(e) {
+//         const node = this.refs.input
+//         const text = node.value.trim()
+//         this.props.onAddClick(text)
+//         node.value = ''
+//     }
+// }
+// //该插件的onAddClick属性必须有函数定义
+// AddTodo.propTypes = {
+//     onAddClick: PropTypes.func.isRequired
+// }
+import React, {Component, PropTypes} from 'react'
+export default class AddTodo extends Component{
+    render(){
         return (
             <div>
-                <input type='text' ref='input' />
-                <button onClick={(e) => this.handleClick(e)}>
+                <input type="text" refs="input" />
+                <button onClick={(e)=>this.handleClick(e)}>
                     Add
                 </button>
             </div>
-        )
+        );
     }
-
-    /**
-     * 将需要添加的内容传给onAddClick然后清空value
-     */
-    handleClick(e) {
-        const node = this.refs.input
-        const text = node.value.trim()
+    handleClick(e){
+        const node=this.refs.input;
+        const text=node.value.trim();
         this.props.onAddClick(text)
-        node.value = ''
+        node.value=''
     }
 }
-
-AddTodo.propTypes = {
+AddTodo.propTypes={
     onAddClick: PropTypes.func.isRequired
 }
--- todoApp/src/components/todo.js
+++ todoApp/src/components/todo.js
@@ -1,25 +1,47 @@
 /**
  * Created by SZL4ZSY on 2/16/2017.
  */
-import React, { Component, PropTypes } from 'react'
+// import React, { Component, PropTypes } from 'react'
+// //这是Todo的每一条数据显示的
+// export default class Todo extends Component {
+//     render() {
+//         return (
+//             <li
+//                 onClick={this.props.onClick}
+//                 style={{
+//                     textDecoration: this.props.completed ? 'line-through' : 'none',
+//                     cursor: this.props.completed ? 'default' : 'pointer'
+//                 }}>
+//                 {this.props.text}
+//             </li>
+//         )
+//     }
+// }
+// //Todo必备属性，onClick函数，text字符串，completed布尔型
+// Todo.propTypes = {
+//     onClick: PropTypes.func.isRequired,
+//     text: PropTypes.string.isRequired,
+//     completed: PropTypes.bool.isRequired
+// }
 
-export default class Todo extends Component {
-    render() {
+import React, { Component,PropTypes } from 'react'
+export default class Todo extends Component{
+    render(){
         return (
             <li
                 onClick={this.props.onClick}
                 style={{
-                    textDecoration: this.props.completed ? 'line-through' : 'none',
-                    cursor: this.props.completed ? 'default' : 'pointer'
-                }}>
+                    textDecoration:this.props.completed?'line-through':'none',
+                    cursor:this.props.completed?'default':'pointer'
+                }}
+            >
                 {this.props.text}
             </li>
         )
     }
 }
-
-Todo.propTypes = {
-    onClick: PropTypes.func.isRequired,
-    text: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired
+Todo.propTypes={
+    onClick:PropTypes.func.isRequired,
+    text:PropTypes.string.isRequired,
+    completed:PropTypes.bool.isRequired
 }
